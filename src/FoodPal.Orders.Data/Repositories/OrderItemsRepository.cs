4235382fb2abc716eb148906cf08ea10ad779a87
Mihalcea Raluca <raluca.mihalcea.e@gmail.com>
2021-02-16 21:55:58 +0200

    Implemented OrderRepository and OrderItemsRepository

--- src/FoodPal.Orders.Data/Repositories/OrderItemsRepository.cs
+++ src/FoodPal.Orders.Data/Repositories/OrderItemsRepository.cs
@@ -1,34 +1,66 @@
 ﻿using FoodPal.Orders.Data.Contracts;
 using FoodPal.Orders.Enums;
 using FoodPal.Orders.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace FoodPal.Orders.Data.Repositories
 {
-	public class OrderItemsRepository : IOrderItemsRepository
-	{
-		private readonly OrdersContext _ordersContext;
+    public class OrderItemsRepository : IOrderItemsRepository
+    {
+        private readonly OrdersContext _ordersContext;
 
-		public OrderItemsRepository(OrdersContext ordersContext)
-		{
-			_ordersContext = ordersContext;
-		}
+        public OrderItemsRepository(OrdersContext ordersContext)
+        {
+            _ordersContext = ordersContext;
+        }
 
-		public async Task<OrderItem> GetOrderItemAsync(int orderId, int orderItemId)
-		{
-			throw new NotImplementedException();
-		}
+        public async Task<OrderItem> GetOrderItemAsync(int orderId, int orderItemId)
+        {
+            try
+            {
+                return await _ordersContext.OrderItems
+                    .Include(o => o.Order).Where(o => o.Id == orderId)
+                    .SingleOrDefaultAsync(o => o.Id == orderItemId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Order Item by orderId: {orderId} and orderItemId: {orderItemId} could not be found. Reason:{ex.Message}");
+            }
+        }
 
-		public async Task<List<OrderItem>> GetItemsAsync(int orderId)
-		{
-			throw new NotImplementedException();
-		}
+        public async Task<List<OrderItem>> GetItemsAsync(int orderId)
+        {
+            try
+            {
+                return await _ordersContext.OrderItems
+                    .Include(o => o.Order).Where(o => o.Id == orderId).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Order Items by orderId: {orderId} could not be found. Reason:{ex.Message}");
+            }
+        }
 
-		public async Task UpdateStatusAsync(OrderItem orderItemEntity, OrderItemStatus newStatus)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public async Task UpdateStatusAsync(OrderItem orderItemEntity, OrderItemStatus newStatus)
+        {
+            try
+            {
+                var orderItem = await _ordersContext.OrderItems.SingleOrDefaultAsync(o => o.Id == orderItemEntity.Id);
+                if (orderItem != null)
+                {
+                    orderItem.Status = newStatus;
+                    _ordersContext.Update(orderItem);
+                    await _ordersContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Order Item by id: {orderItemEntity.Id} could not be updated. Reason:{ex.Message}");
+            }
+        }
+    }
 }
--- src/FoodPal.Orders.Data/Repositories/OrdersRepository.cs
+++ src/FoodPal.Orders.Data/Repositories/OrdersRepository.cs
@@ -1,56 +1,100 @@
 ﻿using FoodPal.Orders.Data.Contracts;
 using FoodPal.Orders.Enums;
 using FoodPal.Orders.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace FoodPal.Orders.Data.Repositories
 {
-	public class OrdersRepository : IOrdersRepository
-	{
-		private readonly OrdersContext _ordersContext;
+    public class OrdersRepository : IOrdersRepository
+    {
+        private readonly OrdersContext _ordersContext;
 
-		public OrdersRepository(OrdersContext ordersContext)
-		{
-			_ordersContext = ordersContext;
-		}
+        public OrdersRepository(OrdersContext ordersContext)
+        {
+            _ordersContext = ordersContext;
+        }
 
-		public async Task<Order> CreateAsync(Order newOrder)
-		{
-			if (newOrder is null) throw new ArgumentNullException(nameof(newOrder));
+        public async Task<Order> CreateAsync(Order newOrder)
+        {
+            if (newOrder is null) throw new ArgumentNullException(nameof(newOrder));
 
-			try
-			{
-				await _ordersContext.AddAsync(newOrder);
-				await _ordersContext.SaveChangesAsync();
+            try
+            {
+                await _ordersContext.AddAsync(newOrder);
+                await _ordersContext.SaveChangesAsync();
 
-				return newOrder;
-			}
-			catch (Exception ex)
-			{
-				throw new Exception($"Order could not be saved. Reason:{ex.Message}");
-			}
-		}
+                return newOrder;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Order could not be saved. Reason:{ex.Message}");
+            }
+        }
 
-		public async Task<Order> GetByIdAsync(int orderId)
-		{
-			throw new NotImplementedException();
-		}
+        public async Task<Order> GetByIdAsync(int orderId)
+        {
+            try
+            {
+                return await _ordersContext.Orders.SingleOrDefaultAsync(x => x.Id == orderId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Order by id {orderId} could not be found. Reason:{ex.Message}");
+            }
+        }
 
-		public async Task<(IEnumerable<Order> Orders, int AllOrdersCount)> GetByFiltersAsync(string customerId, OrderStatus? status, int page, int pageSize)
-		{
-			throw new NotImplementedException();
-		}
+        public async Task<(IEnumerable<Order> Orders, int AllOrdersCount)> GetByFiltersAsync(string customerId, OrderStatus? status, int page, int pageSize)
+        {
+            try
+            {
+                var orders = await _ordersContext.Orders
+                    .Include(o => o.CustomerId)
+                    .Include(o => o.Status)
+                    .Where(o => o.CustomerId == customerId && o.Status == status)
+                    .Skip(pageSize * (page - 1))
+                    .Take(pageSize)
+                    .ToListAsync();
 
-		public async Task<OrderStatus?> GetStatusAsync(int orderId)
-		{
-			throw new NotImplementedException();
-		}
+                return (orders, orders.Count());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Something went wrong. Reason:{ex.Message}");
+            }
+        }
 
-		public async Task UpdateStatusAsync(Order orderEntity, OrderStatus newStatus)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public async Task<OrderStatus?> GetStatusAsync(int orderId)
+        {
+            try
+            {
+                return (await _ordersContext.Orders.Include(o => o.Status).SingleOrDefaultAsync(o => o.Id == orderId)).Status;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Status for order with id: {orderId} could not be found. Reason:{ex.Message}");
+            }
+        }
+
+        public async Task UpdateStatusAsync(Order orderEntity, OrderStatus newStatus)
+        {
+            try
+            {
+                var order = await _ordersContext.Orders.SingleOrDefaultAsync(o => o.Id == orderEntity.Id);
+                if (order != null)
+                {
+                    order.Status = newStatus;
+                    _ordersContext.Update(order);
+                    await _ordersContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Order by id: {orderEntity.Id} could not be updated. Reason:{ex.Message}");
+            }
+        }
+    }
 }
